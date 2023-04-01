c248120458fa1b0c95dba03a91ed8ba1ed6f19b0
taskie <t@skie.jp>
2023-04-02 01:58:34 +0900

    fix: parameters

--- ganji/nn/dcgan.py
+++ ganji/nn/dcgan.py
@@ -106,18 +106,22 @@ class Generator(nn.Module):
                 nn.BatchNorm2d(ngf * 8),
                 nn.ReLU(True),
                 # state size. (ngf*8) x 4 x 4
+                nn.Dropout2d(0.2),
                 nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
                 nn.BatchNorm2d(ngf * 4),
                 nn.ReLU(True),
                 # state size. (ngf*4) x 8 x 8
+                nn.Dropout2d(0.2),
                 nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
                 nn.BatchNorm2d(ngf * 2),
                 nn.ReLU(True),
                 # state size. (ngf*2) x 16 x 16
+                nn.Dropout2d(0.2),
                 nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
                 nn.BatchNorm2d(ngf),
                 nn.ReLU(True),
                 # state size. (ngf) x 32 x 32
+                nn.Dropout2d(0.2),
                 nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
                 nn.Tanh()
                 # state size. (nc) x 64 x 64
@@ -159,18 +163,22 @@ class Discriminator(nn.Module):
                 nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
                 nn.LeakyReLU(0.2, inplace=True),
                 # state size. (ndf) x 32 x 32
+                nn.Dropout2d(0.2),
                 nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
                 nn.BatchNorm2d(ndf * 2),
                 nn.LeakyReLU(0.2, inplace=True),
                 # state size. (ndf*2) x 16 x 16
+                nn.Dropout2d(0.2),
                 nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
                 nn.BatchNorm2d(ndf * 4),
                 nn.LeakyReLU(0.2, inplace=True),
                 # state size. (ndf*4) x 8 x 8
+                nn.Dropout2d(0.2),
                 nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
                 nn.BatchNorm2d(ndf * 8),
                 nn.LeakyReLU(0.2, inplace=True),
                 # state size. (ndf*8) x 4 x 4
+                nn.Dropout2d(0.2),
                 nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
                 nn.Sigmoid(),
             )
@@ -209,7 +217,7 @@ class DCGan:
         self.directory = dir
         self.config, _state = ganji.project.load_metadata(dir)
 
-        self.original = False
+        self.original = True
         if self.original:
             self.image_size = 64
             self.ngf = 64
